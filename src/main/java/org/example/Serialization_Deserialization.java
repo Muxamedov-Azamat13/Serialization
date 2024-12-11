2bca60600433ae9b5b6c72c2770d05e5e48561bd
Muxamedov Azamat <muxamedovazamat13@gmail.com git config --global user.email>
2024-12-11 17:03:20 +0500

    Initial commit

--- src/main/java/org/example/Serialization_Deserialization.java
+++ src/main/java/org/example/Serialization_Deserialization.java
@@ -1,4 +1,73 @@
 package org.example;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+import java.io.*;
+
 public class Serialization_Deserialization {
+
+    public static void serializationObject() throws IOException, ClassNotFoundException {
+        Student student = new Student("John", 25, 12331);
+
+        try (FileOutputStream fileOutputStream = new FileOutputStream("student.bin");
+             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream)) {
+            objectOutputStream.writeObject(student);
+            System.out.println("Объект сериализован!");
+        }
+
+        try (FileInputStream fileInputStream = new FileInputStream("student.bin");
+             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)) {
+            student = (Student) objectInputStream.readObject();
+        }
+
+        System.out.println("Объект дессериализован!" + student);
+        System.out.println("Поле имя: " + student.getName());
+        System.out.println("Поле возраст: " + student.getAge());
+        System.out.println("Поле GPA: (должен быть null так как он помечен trancient!)" + student.getGPA());
+    }
+
+    public static void serializationJSON() {
+        Student student = new Student("James", 22, 213231);
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.writeValue(new File("student.json"), student);
+            System.out.println("Объект сериализован в JSON!");
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            Student deserialization = objectMapper.readValue(new File("student.json"), Student.class);
+            System.out.println("Объект дессериализован из JSON! " + deserialization);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void serializationXML() {
+        Student student = new Student("Brad", 32, 212311);
+
+        try {
+            XmlMapper xmlMapper = new XmlMapper();
+
+            File xmlFile = new File("student.xml");
+            xmlMapper.writeValue(xmlFile, student);
+            System.out.println("Объект сериализован в XML!");
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+
+        try {
+            XmlMapper xmlMapper = new XmlMapper();
+
+            File xmlFile = new File("student.xml");
+            Student deserializationXml = xmlMapper.readValue(xmlFile, Student.class);
+            System.out.println("Объект дессериализован из XML! " + deserializationXml);
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+    }
 }
--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,4 +1,55 @@
 package org.example;
 
-public class Student {
+import java.io.Serial;
+import java.io.Serializable;
+
+public class Student implements Serializable {
+
+    //private static final long serialVersionUID = 1L;
+
+    private String name;
+    private int age;
+    transient double GPA;
+
+    public Student() {
+    }
+
+    public Student(String name, int age, double GPA) {
+        this.name = name;
+        this.age = age;
+        this.GPA = GPA;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setGPA(double GPA) {
+        this.GPA = GPA;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getGPA() {
+        return GPA;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", GPA=" + GPA +
+                '}';
+    }
 }
